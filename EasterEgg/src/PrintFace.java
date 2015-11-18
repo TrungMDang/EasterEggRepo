23fd562ca815262cf29f4b9f184ce304d1108f36
Trung-Dang <Trung-Dang@users.noreply.github.com>
2015-11-17 17:40:38 -0800

    stacie

---
+++ EasterEgg/src/PrintFace.java
@@ -0,0 +1,13 @@
+
+public class PrintFace {
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(" ^  ^");
+		sb.append(" |  |");
+		sb.append(" ____") ;
+		sb.append("|____|") ;
+		return sb.toString();
+	}
+}
